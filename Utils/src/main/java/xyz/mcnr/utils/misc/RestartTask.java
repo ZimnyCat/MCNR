50a4f2088e125dca42014d809b6fce5a9e353353
ZimnyCat <thezimnycat@gmail.com>
2024-10-01 23:20:26 +0500

    упрощён автоперезапуск

--- Utils/src/main/java/xyz/mcnr/utils/misc/RestartTask.java
+++ Utils/src/main/java/xyz/mcnr/utils/misc/RestartTask.java
@@ -4,32 +4,30 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class RestartTask extends BukkitRunnable {
     public long startTime;
     public long restartTime = 14400;
-    boolean[] warns = {true, true, true};
+    List<WarnPair> warns = new LinkedList<>(List.of(
+            new WarnPair(300L, "5 минут"),
+            new WarnPair(60L, "1 минуту"),
+            new WarnPair(10L, "10 секунд")
+    ));
 
     @Override
     public void run() {
-        if ((System.currentTimeMillis() - startTime) > ((restartTime - 400) * 1000)) {
-            if ((System.currentTimeMillis() - startTime) > (restartTime * 1000)) Bukkit.getServer().shutdown();
-            else {
-                warn(0, 300, "5 минут");
-                warn(1, 60, "1 минуту");
-                warn(2, 10, "10 секунд");
-            }
-        }
-    }
-
-    // TODO: переделать (через HashMap<Long, String>?)
-    public void warn(int index, long time, String timeMsg) {
-        if (warns[index] && (System.currentTimeMillis() - startTime) > ((restartTime - time) * 1000)) {
-            warns[index] = false;
-            Bukkit.getServer().broadcastMessage("MCNR перезапустится через " + ChatColor.RED + timeMsg);
+        if (System.currentTimeMillis() - startTime > restartTime * 1000) Bukkit.getServer().shutdown();
+        else if (!warns.isEmpty() && System.currentTimeMillis() - startTime > (restartTime - warns.getFirst().seconds) * 1000) {
+            Bukkit.getServer().broadcastMessage("MCNR перезапустится через " + ChatColor.RED + warns.getFirst().msg);
+            warns.removeFirst();
         }
     }
 
     public void setStartTime(long startTime) {
         this.startTime = startTime;
     }
+
+    record WarnPair(Long seconds, String msg) {}
 }
